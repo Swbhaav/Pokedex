d95f146cf22f625d29a128a08e2a9778717f0936
shovab <shovabk.c11@gmail.com>
2025-07-07 22:15:49 +0545

    detai page

--- lib/pages/pokemonDetail_page.dart
+++ lib/pages/pokemonDetail_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:pokidex/component/my_appbar.dart';
+import 'package:pokidex/component/my_clipper.dart';
 import 'package:pokidex/models/pokemon_model.dart';
 
 class PokemonDetailPage extends StatelessWidget {
@@ -10,7 +13,54 @@ class PokemonDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppbar(title: pokemon.name.toUpperCase()),
+      body: Stack(
+        children: [
+          SizedBox(
+            width: double.infinity,
+          ),
+          scroll(),
+        ],
+      ),
+      );
+  }
 
+  Widget scroll() {
+    return DraggableScrollableSheet(
+      initialChildSize: 0.6,
+      maxChildSize: 1.0,
+      minChildSize: 0.6,
+      builder: (context, scrollController) {
+        return Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 5,
+                        width: 35,
+                        color: Colors.black,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
